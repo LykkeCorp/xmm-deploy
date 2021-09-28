37defc4aa5be9290732c6fc58bf772b5ced9157f
Dvin <dvyu@yandex.ru>
2021-09-28 18:19:36 +0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # xmm-deploy
+Apply all docker-compose yamls to docker host
+# example of usage in pipeline
+```
+name: Deploy Dev VM1
+
+on:
+  push:
+    branches: [ master ]
+    paths:
+    - 'vm1/**'
+
+jobs:
+  deploy:
+    name: deploy
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - name: ssh deploy
+      uses: swisschain/xmm-deploy@master
+      env:
+        DOCKER_VM_HOST: 'root@<IP>'
+        REPOSITORY_VM_DIR: 'vm1'
+        SSH_PRIVATE_KEY: ${{ secrets.DEV_SSH_PRIVATE_KEY }}
+        SSH_KNOW_HOST: '<IP> ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTHAyNTYAAAAIbmAAABBBLOVXiO8fOFnTO/tvMyceRutgJ4pbfsyOfiPB1xZjpNVzmuegG3icr1KFJDf8='
+```
---
+++ entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+# exit when any command fails
+set -e
+
+echo create ssh key
+echo $SSH_PRIVATE_KEY | base64 -d > /id_rsa
+chmod 400 /id_rsa
+echo create ssh know host file
+echo $SSH_KNOW_HOST > /known_hosts
+echo run command
+echo run command
+ssh -i /id_rsa -o UserKnownHostsFile=/known_hosts $DOCKER_VM_HOST << EOF
+  hostname
+  echo pull secrets repository
+  cd xmm-infra-secrets-dev
+  git pull
+  echo pull main repository
+  cd ../xmm-infra-dev
+  git pull
+  cd $REPOSITORY_VM_DIR
+  DC=\$(find . -name docker-compose.yaml)
+  for dc in \$DC
+  do
+    DIR_NAME=\$(echo \$dc | awk -F/ '{print \$2}')
+    echo
+    echo   - = [ \$DIR_NAME ] = -
+    cd \$DIR_NAME
+    pwd
+    ls -la
+    if [ -d ../../../xmm-infra-secrets-dev/\$DIR_NAME ];then
+      echo found secrets folder
+      ls -la ../../../xmm-infra-secrets-dev/\$DIR_NAME
+    fi
+    if [ -f ../../../xmm-infra-secrets-dev/\$DIR_NAME/.env ];then
+      echo found .env file
+      if [ -f .env ];then
+        echo file or symlink exist
+      else
+        echo create symlink
+        ln -s ../../../xmm-infra-secrets-dev/\$DIR_NAME/.env ./.env
+      fi
+    fi
+    echo run service
+    docker-compose up -d
+    echo sleep for 2 seconds
+    sleep 2
+    echo print last logs
+    docker-compose logs
+    cd ..
+  done
+EOF
